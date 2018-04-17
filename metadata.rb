d2c2db104389b3fc845e803930bae11a7180d064
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-04-17 11:38:01 +0400

    Remove nginx version constraint

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 description 'Create Nginx host to redirect to another website'
 long_description ::IO.read(::File.join(::File.dirname(__FILE__), 'README.md'))
-version '1.2.0'
+version '1.3.0'
 
 scm_url = 'https://github.com/aspyatkin/redirect-cookbook'
 source_url scm_url if respond_to?(:source_url)
 issues_url "#{scm_url}/issues" if respond_to?(:issues_url)
 
-depends 'nginx', '~> 7.0.0'
+depends 'nginx'
 depends 'tls', '~> 3.0.0'
